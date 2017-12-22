04143fd03bc3a31a9679a3daafa8c0f5c85aaaad
Adeyenuwo, Paul <Paul.Adeyenuwo@mastercard.com>
2017-12-22 15:22:30 -0500

    Integrate Zipkin server and integrate apps with Zipkin

---
+++ platform-services/zipkin-server/src/main/java/io/pivotal/pal/tracker/zipkin/App.java
@@ -0,0 +1,21 @@
+package io.pivotal.pal.tracker.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+import java.util.TimeZone;
+
+/**
+ * Created by e063106 on 12/22/17.
+ */
+
+@EnableZipkinStreamServer
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(App.class, args);
+    }
+}
